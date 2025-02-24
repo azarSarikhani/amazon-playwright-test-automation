a4e579566ecba74b8821b2111044dc47082b3448
Azar <azar.sarikhani.agh@gmail.com>
2025-02-24 08:47:33 +0200

    remove other browsers, add chromium only

--- tests/Nikon-amazon.spec.ts
+++ tests/Nikon-amazon.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from "@playwright/test";
+import { chromium } from "playwright";
 
-test("test", async ({ page }) => {
+test("test", async ({}) => {
+	const browser = await chromium.launch();
+	const context = await browser.newContext();
+	context.setDefaultTimeout(120000);
+	const page = await context.newPage();
 	const getRandomDelay = () => Math.random() * (153) + 67;
 	const getRandomMouseMove = async () => {
 		await page.mouse.move(Math.random() * 1000, Math.random() * 800);
-		await page.waitForTimeout(getRandomDelay());
 	};
 	await page.goto("https://www.amazon.com/");
 	await getRandomMouseMove();
 	await getRandomMouseMove();
-	await page.waitForSelector('input#twotabsearchtextbox', { state: 'visible', timeout: 100000 });
+	// await page.waitForSelector('input#twotabsearchtextbox', { state: 'visible', timeout: 100000 });
 	const searchBox = page.locator("input#twotabsearchtextbox");
 	await searchBox.fill("Nikon");
 	await getRandomMouseMove();
@@ -17,13 +21,15 @@ test("test", async ({ page }) => {
 	await page.locator("input#nav-search-submit-button").click({ delay: getRandomDelay() });
 	await getRandomMouseMove();
 	await getRandomMouseMove();
-	await page.waitForSelector('[aria-label="Sort by"]', { state: "visible", timeout: 10000 });
-	await page.locator("#a-autoid-46").click();
+	// await page.waitForSelector('[aria-label="Sort by"]', { state: "visible", timeout: 10000 });
+	await page.evaluate(() => {
+		window.scrollBy(0, window.innerHeight);
+	  });
+	await page.getByText("Sort by:Featured").click({ delay: getRandomDelay() });
 	await page
-		.getByRole("option", { name: "Price: High to Low" })
-		.locator("#s-result-sort-select_2")
-		.click({delay: getRandomDelay()});
-	
+		.getByLabel("Price: High to Low")
+		.getByText("Price: High to Low")
+		.click({ delay: getRandomDelay() });
 	await page.waitForTimeout(getRandomDelay());
 	await page.mouse.wheel(0, 500);
 	await page.waitForTimeout(getRandomDelay());
@@ -45,4 +51,5 @@ test("test", async ({ page }) => {
 	// Verify that the product title includes "Nikon D3X"
 	const productTitle = await page.locator('#productTitle').textContent();
 	expect(productTitle).toContain('Nikon D3X');
+	await browser.close();
 });
