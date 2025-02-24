3dcb0a511a0cd1d829d831e74fc98aaa039dfc22
Azar <azar.sarikhani.agh@gmail.com>
2025-02-24 10:33:45 +0200

    remove mouse movements and delays. Fix tests for chromium and webkit, but not firefox.

--- tests/Nikon-amazon.spec.ts
+++ tests/Nikon-amazon.spec.ts
@@ -1,55 +1,14 @@
 import { test, expect } from "@playwright/test";
-import { chromium } from "playwright";
 
-test("test", async ({}) => {
-	const browser = await chromium.launch();
-	const context = await browser.newContext();
-	context.setDefaultTimeout(120000);
-	const page = await context.newPage();
-	const getRandomDelay = () => Math.random() * (153) + 67;
-	const getRandomMouseMove = async () => {
-		await page.mouse.move(Math.random() * 1000, Math.random() * 800);
-	};
+test("test", async ({page}) => {
 	await page.goto("https://www.amazon.com/");
-	await getRandomMouseMove();
-	await getRandomMouseMove();
-	// await page.waitForSelector('input#twotabsearchtextbox', { state: 'visible', timeout: 100000 });
 	const searchBox = page.locator("input#twotabsearchtextbox");
 	await searchBox.fill("Nikon");
-	await getRandomMouseMove();
-	await getRandomMouseMove();
-	await page.locator("input#nav-search-submit-button").click({ delay: getRandomDelay() });
-	await getRandomMouseMove();
-	await getRandomMouseMove();
-	// await page.waitForSelector('[aria-label="Sort by"]', { state: "visible", timeout: 10000 });
-	await page.evaluate(() => {
-		window.scrollBy(0, window.innerHeight);
-	  });
-	await page.getByText("Sort by:Featured").click({ delay: getRandomDelay() });
-	await page
-		.getByLabel("Price: High to Low")
-		.getByText("Price: High to Low")
-		.click({ delay: getRandomDelay() });
-	await page.waitForTimeout(getRandomDelay());
-	await page.mouse.wheel(0, 500);
-	await page.waitForTimeout(getRandomDelay());
-	await page.mouse.wheel(0, -500); // Scroll up
-
-	const secondProduct = page
-		.locator('[data-component-type="s-search-result"]')
-		.nth(1);
-	await secondProduct.waitFor();
-	await secondProduct.click({ delay: getRandomDelay() });
-	await page.waitForTimeout(getRandomDelay());
-	await page.mouse.wheel(0, 500);
-	await page.waitForTimeout(getRandomDelay());
-	await page.mouse.wheel(0, -500); // Scroll up
+	await page.locator("input#nav-search-submit-button").click();
+	await page.locator('#a-autoid-0').click();
+	await page.getByRole('option', { name: 'Price: High to Low' }).locator('#s-result-sort-select_2').click();
+	await page.locator('.s-product-image-container').nth(1).click();
 	await page.getByRole("link", { name: "See more product details" }).click();
-	await page.waitForTimeout(getRandomDelay());
-	await page.getByText("Product information Item").click();
 	await page.getByRole("button", { name: "Item details" }).click();
-	// Verify that the product title includes "Nikon D3X"
-	const productTitle = await page.locator('#productTitle').textContent();
-	expect(productTitle).toContain('Nikon D3X');
-	await browser.close();
+  	await expect(page.locator('#productDetails_expanderTables_depthLeftSections')).toContainText('D6 FX-Format');
 });
