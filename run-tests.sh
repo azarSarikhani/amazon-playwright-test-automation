2be65d33d8c604f1bfaf46104d5339cd682f4a67
Azar <azar.sarikhani.agh@gmail.com>
2025-02-24 20:28:47 +0200

    fix report show when tests fail in docker

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Run Playwright tests
-npx playwright test --reporter=html
+npx playwright test
 
-# Show the test report
-npx playwright show-report --host 0.0.0.0 --port 9323
+npx playwright show-report --host 0.0.0.0 --port 9323 
 
 # Keep the container running
 tail -f /dev/null
