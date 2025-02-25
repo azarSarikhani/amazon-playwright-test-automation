4bac1ad44ee1b7741c9b7e2092747c2e4cb17287
Azar <azar.sarikhani.agh@gmail.com>
2025-02-25 08:15:04 +0200

    make URL configurable. add alternative to searchbox

--- tests/Nikon-amazon.spec.ts
+++ tests/Nikon-amazon.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from "@playwright/test";
 
+const baseURL = process.env.BASE_URL || "https://www.amazon.com/";
+
 test("test", async ({page}) => {
-	await page.goto("https://www.amazon.com/");
+	await page.goto(baseURL);
 	const searchBox = page.locator("input#twotabsearchtextbox");
+	if (!(await searchBox.isVisible())) {
+		const searchBox = page.getByRole('textbox', { name: 'Search For' });
+	}
+	if (!(await searchBox.isVisible())) {
+		const searchBox = page.locator("input#nav-bb-search");
+	}
 	await searchBox.fill("Nikon");
 	await page.locator("input#nav-search-submit-button").click();
 	await page.locator('#a-autoid-0').click();
@@ -10,5 +18,8 @@ test("test", async ({page}) => {
 	await page.locator('.s-product-image-container').nth(1).click();
 	await page.getByRole("link", { name: "See more product details" }).click();
 	await page.getByRole("button", { name: "Item details" }).click();
-  	await expect(page.locator('#productDetails_expanderTables_depthLeftSections')).toContainText('D6 FX-Format');
+	//await expect(page.locator('#productDetails_expanderTables_depthLeftSections')).toContainText('Nikon D3X');
+	//To make the tests pass comment the above line and uncomment the bellow line,
+	//At the time of writting this file the second choice in search list was Nikon D6 and not D3X
+  	await expect(page.locator('#productDetails_expanderTables_depthLeftSections')).toContainText('D6 FX-Format');  
 });
